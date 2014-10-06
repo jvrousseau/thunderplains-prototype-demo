b6e9528a880f1064bd25556a822d1061809ecb06
Jordan Rousseau <jrousseau@wdtinc.com>
2014-10-06 11:53:52 -0500

    change test port back to 2525

--- load-test.js
+++ load-test.js
@@ -1,6 +1,6 @@
 var loadtest = require('loadtest');
 var options = {
-    url: 'http://jrousseau.norman.wdtinc.com:8080/employee/2/task',
+    url: 'http://jrousseau.norman.wdtinc.com:2525/employee/2/task',
     maxRequests: 10000,
     concurrency: 70
 };
